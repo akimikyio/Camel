5f83a13a1080a365ce2d6cc1947d1cc3a5cade3c
Akimi Kyio <zils.gleb@gmail.com>
2024-07-03 19:54:57 +0700

    Reworked movement mechanics and added test sprites

--- elements/Camel/camel.gd
+++ elements/Camel/camel.gd
@@ -1,14 +1,22 @@
 extends CharacterBody2D
 
-const SPEED = 100
-const SPEED_COEFF = 0.5;
-var direction = Vector2.RIGHT
+const SPEED = 100.0
+const JUMP_VELOCITY = -200.0
 
-func _process(_delta):
-	velocity = SPEED * direction
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta):
 	
-	if Input.is_action_pressed("left"):
-		velocity *= 1 - SPEED_COEFF
-	if Input.is_action_pressed("right"):
-		velocity *= 1 + SPEED_COEFF
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	var direction = Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
 	move_and_slide()
