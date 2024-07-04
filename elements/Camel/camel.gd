3aef78742e2f474bb6f2662a43e7ba10b7eb1b18
Akimi Kyio <zils.gleb@gmail.com>
2024-07-04 07:35:04 +0700

    working on parallax background; commented movement code

--- elements/Camel/camel.gd
+++ elements/Camel/camel.gd
@@ -1,17 +1,17 @@
 extends CharacterBody2D
 
-const SPEED = 100.0
-const JUMP_VELOCITY = -200.0
+const SPEED: float = 100.0
+const JUMP_VELOCITY: float = -200.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-func _physics_process(delta):
+func _physics_process(delta: float):
 	
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y += gravity * delta 	# affecting character by gravity
 	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = JUMP_VELOCITY	# lifting up a character by the amount of px in JUMP_VELOCITY
 		
 	var direction = Input.get_axis("left", "right")
 	if direction:
